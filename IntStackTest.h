3284c374f09d43c31aefc67c5fbf3ac104208393
bthr-J <33852442+bthr-J@users.noreply.github.com>
2018-04-20 17:49:50 +0900

    book

---
+++ IntStackTest.h
@@ -0,0 +1,14 @@
+//
+//  IntStackTest.h
+//  dataS
+//
+//  Created by joo sunghyun on 2018. 4. 20..
+//  Copyright © 2018년 os_work. All rights reserved.
+//
+
+#ifndef IntStackTest_h
+#define IntStackTest_h
+
+#include <stdio.h>
+
+#endif /* IntStackTest_h */
---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dataS
+//
+//  Created by joo sunghyun on 2018. 4. 1..
+//  Copyright © 2018년 os_work. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
